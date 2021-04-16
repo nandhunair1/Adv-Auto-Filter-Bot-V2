e2b5e95cce9d65b47ed71232bfabb68a82767c6b
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-04-16 23:59:04 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1367,14 +1367,14 @@ async def callback_data(bot, update: CallbackQuery):
         text=f"<i><u>Bot's Status</u></i>\n"
         text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
         text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-        text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-        text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/AlbertEinsteinTG/Adv-Filter-Bot-V2">Source</a>"""
+        text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/tvseriezzz">@tvseriezzz</a>\n"""
+        text+="""\n<b><i>Support:</i></b> <a href="https://tvseriezzz">Source</a>"""
 
         buttons = [
             [
                 InlineKeyboardButton
                     (
-                        "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                        "My Dev ⚡", url="https://t.me/MrC_VENOM"
                     ),
                     
                 InlineKeyboardButton
@@ -1399,10 +1399,10 @@ async def callback_data(bot, update: CallbackQuery):
         
     elif query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/MrC_VENOM'),
+            InlineKeyboardButton('Group 🧾', url ='https://t.me/MrCVENOM_Chat')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Support 🛠', url='https://t.me/tvseriezzz')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
