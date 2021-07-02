864a620eb47bb145414e5f6f5b0acd044277396b
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-07-02 12:24:32 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("𝙽𝚎𝚡𝚝»»»", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"<b>🗂️Total File :- {(len_results)}</b>\n<b>🎬Movie Name :-</b> <code>{query}</code>",
+                text=f"<b>🗂️ Total Files Found :- {(len_results)}</b>\n<b>📑 Requested Query :-</b> <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
