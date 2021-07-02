ba4cb7835d4066866a3a7121eb9f45bc78e9185b
nandhunair1 <77907590+nandhunair1@users.noreply.github.com>
2021-07-02 12:50:29 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -95,7 +95,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"📑 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 📑", callback_data="ignore")
+            InlineKeyboardButton(f"⚜ Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} ⚜", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<b>🗂️ Total Files Found :- {leng}</b>\n<b>🎬Requested Query :-</b> <code>{query}</code>"
+    text=f"<b>🗂️ Total Files Found :- {leng}</b>\n<b>🎬 Requested Query :-</b> <code>{query}</code>"
         
     try:
         await update.message.edit(
